a81ef6502d10f001117d3d39e00a3019f543fdff
Jason Jordan <jasonajordan0@gmail.com>
2020-10-03 15:10:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,7 +36,7 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   
-  first
+  first = nil
   isTrue = false
   planeteer_calls.each do |string|
     if string.length > 4  && isTrue == false
